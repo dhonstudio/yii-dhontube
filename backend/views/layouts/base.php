043c2f631b6d407b868826e428abd7a696283853
dhonstudio <doon13@gmail.com>
2022-01-26 14:22:32 +0700

    fixed layout

---
+++ backend/views/layouts/base.php
@@ -0,0 +1,36 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use backend\assets\AppAsset;
+use common\widgets\Alert;
+use yii\bootstrap4\Breadcrumbs;
+use yii\bootstrap4\Html;
+use yii\bootstrap4\Nav;
+use yii\bootstrap4\NavBar;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>" class="h-100">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap h-100 d-flex flex-column">
+    <?= $this->render('_header') ?>
+    <?= $content?>
+</div>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage();
--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -11,34 +11,16 @@ use yii\bootstrap4\Nav;
 use yii\bootstrap4\NavBar;
 
 AppAsset::register($this);
+$this->beginContent('@backend/views/layouts/base.php');
 ?>
-<?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>" class="h-100">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <?php $this->registerCsrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body>
-<?php $this->beginBody() ?>
 
-<div class="wrap h-100 d-flex flex-column">
-    <?= $this->render('_header') ?>
-    
-    <main class="d-flex">
-        <?= $this->render('_sidebar') ?>
+<main class="d-flex">
+    <?= $this->render('_sidebar') ?>
 
-        <div class="content-wrapper p-3">
-            <?= Alert::widget() ?>
-            <?= $content ?>
-        </div>
-    </main>
-</div>
+    <div class="content-wrapper p-3">
+        <?= Alert::widget() ?>
+        <?= $content ?>
+    </div>
+</main>
 
-<?php $this->endBody() ?>
-</body>
-</html>
-<?php $this->endPage();
+<?= $this->endContent();?>
